bc13fc391bb06ebefa7cb57dafd1921c619ab904
Joonas Javanainen <joonas.javanainen@gmail.com>
2012-09-02 15:28:24 +0300

    Fix: Prevent ConcurrentModificationExceptions with EventSources

--- frp/src/main/java/fi/jawsy/jawwa/frp/EventSource.java
+++ frp/src/main/java/fi/jawsy/jawwa/frp/EventSource.java
@@ -1,9 +1,9 @@
 package fi.jawsy.jawwa.frp;
 
 import java.io.Serializable;
-import java.util.Set;
 
-import com.google.common.collect.Sets;
+import com.google.common.base.Objects;
+import com.google.common.collect.ImmutableSet;
 
 import fi.jawsy.jawwa.lang.Effect;
 
@@ -11,20 +11,39 @@ public class EventSource<E> extends EventStreamBase<E> implements EventSink<E> {
 
     private static final long serialVersionUID = -2046636095523847664L;
 
-    private final Set<Effect<? super E>> listeners = Sets.newIdentityHashSet();
+    private ImmutableSet<Effect<? super E>> listeners = ImmutableSet.of();
 
     @Override
     public CleanupHandle foreach(final Effect<? super E> e) {
-        listeners.add(e);
+        addListener(e);
         class ListenerCleanup implements CleanupHandle, Serializable {
             private static final long serialVersionUID = -8501830733777586751L;
 
             @Override
             public void cleanup() {
-                listeners.remove(e);
+                removeListener(e);
             }
         }
         return new ListenerCleanup();
+    }
+
+    void removeListener(Effect<? super E> e) {
+        ImmutableSet.Builder<Effect<? super E>> b = ImmutableSet.builder();
+
+        for (Effect<? super E> listener : listeners) {
+            if (!Objects.equal(e, listener))
+                b.add(listener);
+        }
+
+        listeners = b.build();
+    }
+
+    void addListener(Effect<? super E> e) {
+        ImmutableSet.Builder<Effect<? super E>> b = ImmutableSet.builder();
+
+        b.addAll(listeners).add(e);
+
+        listeners = b.build();
     }
 
     public static <E> EventSource<E> create() {
